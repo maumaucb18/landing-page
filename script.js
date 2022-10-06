97f13a6fc7c55fe658bda6965dd45930fe88ba2c
mauricio pignoli vargas <62521025+maumaucb18@users.noreply.github.com>
2022-10-05 22:14:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function imgSlider(anything){
+ document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color){
+  const Circle = document.querySelector('.circle');
+  Circle.style.background = color;
+}
